7bcc5bbb0cd84456e743a6303a4ee40d80e071eb
jzangarinim <126709250+jzangarinim@users.noreply.github.com>
2023-06-20 23:00:56 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -1,32 +1,56 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from datetime import datetime
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    username = Column(String(20), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    created_at = Column(DateTime(timezone=False))
+    password = Column(String(30), nullable=False)
+    # relationship(nombre de la CLASE)
+    favorite_list = relationship('Favorite', back_populates='user', uselist=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    def __str__(self):
+        return self.username
+
+class Favorite(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=True)
+    planets_id = Column(Integer, ForeignKey('planets.id'), nullable=True)
+    
+class Character(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    eye_color = Column(String, nullable=False)
+    hair_color = Column(String, nullable=False)
+    height = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
+    
+    def __str__(self):
+        return self.name
 
-    def to_dict(self):
-        return {}
+class Planet(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    population = Column(Integer, nullable=False)
+    climate = Column(String, nullable=False)
+    terrain = Column(String, nullable=False)
+    
+    def __str__(self):
+        return self.name
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
